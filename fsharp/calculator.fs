b7e8287bddd6451ddaef4735bc35acf041f46a0a
Tony Vo <ttrung.vo@gmail.com>
2023-03-25 14:07:31 -0600

    initial design

--- fsharp/calculator.fs
+++ fsharp/calculator.fs
@@ -1,3 +1,47 @@
 module SampleHelloWorld.HelloWorld
 
-let square x = x*x;
+type Calculate = CalculatorInput * CalculatorState -> CalculatorState
+and CalculatorState = {
+    display: CalculatorDisplay
+    pendingOp: (CalculatorMathOp * Number) option
+    }
+and CalculatorDisplay = string
+and CalculatorInput =
+    | Digit of CalculatorDigit
+    | Op of CalculatorMathOp
+    | Action of CalculatorAction
+and CalculatorDigit =
+    | Zero | One | Two | Three | Four
+    | Five | Six | Seven | Eight | Nine
+    | DecimalSeparator
+and CalculatorMathOp =
+    | Add | Subtract | Multiply | Divide
+and CalculatorAction =
+    | Equals | Clear
+and UpdateDisplayFromDigit =
+    CalculatorDigit * CalculatorDisplay -> CalculatorDisplay
+
+and DoMathOperation =
+    CalculatorMathOp * Number * Number -> MathOperationResult
+and Number = float
+and MathOperationResult =
+    | Success of Number
+    | Failure of MathOperationError
+and MathOperationError =
+    | DivideByZero
+
+type GetDisplayNumber =
+    CalculatorDisplay -> Number option
+type SetDisplayNumber =
+    Number -> CalculatorDisplay
+
+type InitState =
+    unit -> CalculatorState
+
+type CalculatorServices = {
+    updateDisplayFromDigit: UpdateDisplayFromDigit
+    doMathOperation: DoMathOperation
+    getDisplayNumber: GetDisplayNumber
+    setDisplayNumber: SetDisplayNumber
+    initState: InitState
+    }
---
+++ fsharp/retro.md
@@ -0,0 +1,14 @@
+- what is * means -> it's means combing input.
+- it's hard to understand the language when we haven't worked with the language before
+
+- liked
+  - skipping/copying the entire code instead of copying one by one.
+    - summary first,
+  - how we follow the walk through
+  - it's seems to be hard for facilitator.
+
+- learned
+  - we learned f# a little bit. +1
+  - we tried something new that we haven't done before, where we haven't tried f#
+  - Effectiveness for typist (GitPod, Live Share, Code w/ Me, etc.)
+  - some f# interface design
