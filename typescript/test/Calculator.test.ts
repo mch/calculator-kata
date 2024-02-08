b2b026f34d5f0ad0130362018aaeaa208a057e8e
Tony Trung Thanh Vo <ttrung.vo@gmail.com>
2024-02-08 19:23:22 +0000

    add comments

--- typescript/test/Calculator.test.ts
+++ typescript/test/Calculator.test.ts
@@ -1,6 +1,8 @@
 import { describe, test } from "@jest/globals";
 import { Calculator } from "../src/Calculator";
 
+// these tests must be passing in order for the calculator to work
+// todo add more test cases
 
 describe("Calculator tests", () => {
   test("move", () => {});
