3b908a19afec7cfc143a2e8f1c4996c4de54fd50
Tony Vo <ttrung.vo@gmail.com>
2024-06-08 08:50:07 -0600

    initial calculator interface

--- typescript/src/Calculator.ts
+++ typescript/src/Calculator.ts
@@ -1,11 +1,31 @@
-class Digit {
+enum CalculatorInput {
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    DecimalSeparator,
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Equals,
+    Clear
+}
+type CalculatorOutput = unknown
+type CalculatorState = {
+    display: CalculatorDisplay
+}
+type CalculatorDisplay = string
 
+
+export function calculate (input: CalculatorInput, state: CalculatorState) : CalculatorOutput {
+    return undefined
 }
 
-export class Calculator {
-    displayOnScreen(digit: Digit): String {
-        return "";
-    }
-
-}
 
