7c22ee4af86fd14c5d3b2e7f2450c554fffa2fef
Tony Vo <ttrung.vo@gmail.com>
2024-06-08 08:59:16 -0600

    add additional types operations/actions/digit

--- typescript/src/Calculator.ts
+++ typescript/src/Calculator.ts
@@ -22,10 +22,35 @@ type CalculatorState = {
     display: CalculatorDisplay
 }
 type CalculatorDisplay = string
-
+enum CalculatorDigit {
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    DecimalSeparator
+}
+enum CalculatorOperation {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+}
+enum CalculatorAction {
+    Equals,
+    Clear
+}
 
 export function calculate (input: CalculatorInput, state: CalculatorState) : CalculatorOutput {
     return undefined
 }
 
+export function updateDisplayFromDigit (digit: CalculatorDigit, display: CalculatorDisplay) : CalculatorDisplay {
+    return display
+}
 
